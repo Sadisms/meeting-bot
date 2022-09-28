6acb7b0dd9f220e506bdbaf3f8c7ca229befcc23
дмитрий Madara <madara@deeplay.io>
2022-09-28 13:05:32 +0600

    Edit logging

--- utils/logging.py
+++ utils/logging.py
@@ -1,24 +1,12 @@
 import logging
-from logging.handlers import TimedRotatingFileHandler
-from pathlib import Path
-
 
 formatter = logging.Formatter('%(filename)-15s %(name)-8s LINE:%(lineno)-4d '
                               f'%(levelname)-8s [%(asctime)s] > %(message)s')
 
 
-def create_logger(name, logger='', path=f"logs", level=logging.INFO, console=True):
-    Path(path).mkdir(parents=True, exist_ok=True)
-
-    handler = logging.handlers.TimedRotatingFileHandler(
-        filename=f'{path}\\{name}.log', when='W0'
-    )
-    handler.setFormatter(formatter)
-
+def create_logger(logger='', level=logging.INFO, console=True):
     logger = logging.getLogger(logger)
     logger.setLevel(level)
-
-    logger.addHandler(handler)
 
     if console:
         console_out = logging.StreamHandler()
@@ -28,5 +16,5 @@ def create_logger(name, logger='', path=f"logs", level=logging.INFO, console=Tru
     return logger
 
 
-create_logger('slack_bolt', 'slack_bolt.AsyncApp')
-slack_logging = create_logger('support_log', 'support')
+create_logger('slack_bolt.AsyncApp')
+slack_logging = create_logger('meeting')
