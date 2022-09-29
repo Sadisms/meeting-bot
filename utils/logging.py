70bbf8a38569902f0c5dac71db2c9ab3db780f45
дмитрий Madara <madara@deeplay.io>
2022-09-29 14:44:22 +0600

    Add debug

--- utils/logging.py
+++ utils/logging.py
@@ -1,12 +1,14 @@
 import logging
 
+from data.config import DEBUG
+
 formatter = logging.Formatter('%(filename)-15s %(name)-8s LINE:%(lineno)-4d '
                               f'%(levelname)-8s [%(asctime)s] > %(message)s')
 
 
 def create_logger(logger='', level=logging.INFO, console=True):
     logger = logging.getLogger(logger)
-    logger.setLevel(level)
+    logger.setLevel(level if not DEBUG else logging.DEBUG)
 
     if console:
         console_out = logging.StreamHandler()
